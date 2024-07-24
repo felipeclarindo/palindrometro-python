e402291b00f5b8b272e91e0c63255b1ecadbe2b3
felipeclarindo <fglpc2@gmail.com>
2024-07-24 02:55:32 -0300

    Finalizando funcionalidades e configurações

--- src/main.py
+++ src/main.py
@@ -1,2 +1,4 @@
-from modules import *
-from customtkinter import *
+from modules.gui import GUI # Importa a interface gráfica do módulo gui localizado na pasta modules
+
+screen = GUI() # Cria uma instancia da classe GUI que inicializa a interface gráfica da aplicação
+screen.executar() # Chama o método 'executar' da instancia 'screen', que executa a interface gráfica e inicia o loop da aplicação 
--- src/modules/verificarPalindromo.py
+++
@@ -1,5 +0,0 @@
-def verificarPalindromo(palavra):
-    ehpalindromo = False
-    if palavra.lower()[::-1] == palavra.lower():
-        ehpalindromo = True
-    return ehpalindromo
